592e810aedd031591698cc41e82dbd961650bf6a
Yajaira Hercules <yajairah@student.21-school.ru>
2023-03-28 13:08:29 +0300

    remove ** pointer and add static

--- src/state_sort.c
+++ src/state_sort.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void menu(int choice);
-char* input();
-void prFile(char *path);
-void sortFile(char *path);
-void addLine(char *path);
-
 typedef struct line {
     int year;
     int month;
@@ -18,6 +12,14 @@ typedef struct line {
     int code;
 } Line;
 
+void menu(int choice);
+char *input();
+void prFile(char *path);
+void sortFile(char *path);
+void addLine(char *path);
+/* void destroy(Line **data, int counterStruct); */
+
+
 int main(void) {
     int n, trash;
     if ((scanf("%d.%d", &n, &trash)) != 1) {
@@ -25,7 +27,7 @@ int main(void) {
     } else {
         menu(n);
     }
-    
+
     return (0);
 }
 
@@ -45,66 +47,48 @@ void menu(int n) {
     free(path);
 }
 
-char* input() {
-    char* path = malloc(sizeof(char) * 250);
+char *input() {
+    char *path = malloc(sizeof(char) * 250);
     scanf("%249s", path);
     return (path);
 }
 
 void prFile(char *path) {
-    FILE * fp = fopen(path, "rb");
-    /* int counterFields = 0; */
+    FILE *fp = fopen(path, "rb");
     int counterStruct = 0;
-    Line* value = malloc(sizeof(Line));;
-    Line* data = malloc(sizeof(Line*) * 5000);
+    Line *data = (Line *)malloc(sizeof(Line) * 5000);
+    /* Line value; */
 
-    while (fread(&value, sizeof(int), 1, fp)) {
-        /* if (counterFields == 8) { */
-        /*     counterStruct++; */
-        /*     counterFields = 0; */
-        /* } */ 
-        data[counterStruct].year = value.year;
-        data[counterStruct].month = value.month;
-        data[counterStruct].day = value.day;
-        data[counterStruct].hour = value.hour;
-        data[counterStruct].min = value.min;
-        data[counterStruct].sec = value.sec;
-        data[counterStruct].status = value.status;
-        data[counterStruct].code = value.code;
-        /* counterFields++; */
-        printf("%5d %5d %5d %5d %5d %5d %5d %5d\n", data[counterStruct].year, data[counterStruct].month, data[counterStruct].day, data[counterStruct].hour, data[counterStruct].min, data[counterStruct].sec, data[counterStruct].status, data[counterStruct].code);
-        counterStruct++;
-        }
+    while (fread(&data[counterStruct], sizeof(Line), 1, fp)) {
+        printf("%d %d %d %d %d %d %d %d\n", data[counterStruct].year, data[counterStruct].month,
+               data[counterStruct].day, data[counterStruct].hour, data[counterStruct].min,
+               data[counterStruct].sec, data[counterStruct].status, data[counterStruct].code);
+    }
 
-            /* switch (counterFields++) { */
-            /*     case 0: */
-            /*         data[counterStruct].year = value.year; */
-            /*         break; */
-            /*     case 1: */
-            /*         data[counterStruct].month = value.month; */
-            /*         break; */
-            /*     case 2: */
-            /*         data[counterStruct].day = value.day; */
-            /*         break; */
-            /*     case 3: */
-            /*         data[counterStruct].hour = value.hour; */
-            /*         break; */
-            /*     case 4: */
-            /*         data[counterStruct].min = value.min; */
-            /*         break; */
-            /*     case 5: */
-            /*         data[counterStruct].sec = value.sec; */
-            /*         break; */
-            /*     case 6: */
-            /*         data[counterStruct].status = value.status; */
-            /*         break; */
-            /*     case 7: */
-            /*         data[counterStruct].code = value.code; */
-            /*         break; */
-            /* } */
+    /* while (fread(&value, sizeof(Line), 1, fp)) { */
+    /*     data[counterStruct] = malloc(sizeof(Line)); */
+    /*     data[counterStruct]->year = value.year; */
+    /*     data[counterStruct]->month = value.month; */
+    /*     data[counterStruct]->day = value.day; */
+    /*     data[counterStruct]->hour = value.hour; */
+    /*     data[counterStruct]->min = value.min; */
+    /*     data[counterStruct]->sec = value.sec; */
+    /*     data[counterStruct]->status = value.status; */
+    /*     data[counterStruct]->code = value.code; */
+    /*     printf("%d %d %d %d %d %d %d %d\n", data[counterStruct]->year, data[counterStruct]->month, */
+    /*            data[counterStruct]->day, data[counterStruct]->hour, data[counterStruct]->min, */
+    /*            data[counterStruct]->sec, data[counterStruct]->status, data[counterStruct]->code); */
+    /* } */
     fclose(fp);
+    free(data);
+    /* destroy(data, counterStruct); */
+    
 }
 
-
-
+/* void destroy(Line **data, int counterStruct) { */
+/*     while (counterStruct-- != 0) { */
+/*         free(data[counterStruct]); */
+/*     } */
+/*     free(data); */
+/* } */
 
